5593a058859e17e8b9b0989d97b5303f5b5ab6c8
umutkavakli <umutkavakli.ukk@gmail.com>
2023-03-04 05:21:16 +0300

    scraper errors are fixed and it can be used now.

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
+import os
+import time
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+
 
 class Scraper:
 
@@ -12,96 +15,114 @@ class Scraper:
     __BASE_URL = 'https://www.sikayetvar.com'
     __QUERY = '?page='
 
-    def __init__(self, brands, complaint_type, start_page, end_page):
+    def __init__(self, brands, start_page, end_page, info, file_extension, output_path):
         self.brands = brands
-        self.complaint_type = complaint_type
         self.start_page = start_page
         self.end_page = end_page
+        self.info = info
+        self.file_extension = file_extension
+        self.output_path = output_path
         self.rows = []
 
+    def change_page(self, brand, page_number):
+        """
+        Returns the soup of brand page in current page number.
+        """
 
-    def change_page(self, brand, complaint=None):
-        if complaint is not None:
-            next_query = Scraper.__BASE_URL + complaint.find('section').find('h2').find('a').get('href')
-        else:
-            next_query = Scraper.__BASE_URL + '/' + brand.lower() + Scraper.__QUERY + str(self.page_number)
-
+        next_query = Scraper.__BASE_URL + '/' + brand.lower() + Scraper.__QUERY + str(page_number)
         next_page = requests.get(next_query, headers=Scraper.__HEADERS)
         return BeautifulSoup(next_page.content, 'html.parser')
-
-
-    def add_unsolved(self, brand, page_soup):
-        unsolved = page_soup.findAll('article', {'class': 'story-card complaint-card ga-v ga-c'})
-
-        for i in range(len(unsolved)):
-            complaint_soup = self.change_page(brand, complaint=unsolved[i])
-            complaint = complaint_soup.find('article', {'class': 'story-primary complaint-card'})
-
-            data = {
-                'brand':           brand,
-                'customer_name':   complaint.find('span', {'class': 'username'}).find('a').text,
-                'customer_link':   complaint.find('span', {'class': 'username'}).find('a').get('href'),
-                'date':            complaint.find('span', {'class': 'username'}).find('span', {'class': 'post-time'}).get('title'),
-                'rating':          len(complaint_soup.find('section', {'class': 'post-setting'}).findAll('span', {'class': 'dark-yellow icomoon-full-star'})),
-                'is_solved':       False,
-                'complaint_title': complaint.find('h1', {'class': 'complaint-title'}).text,
-                'complaint_text':  complaint.find('div', {'class': 'card-text'}).text
-            }
-
-            self.rows.append(data)
     
+    def change_complaint_card(self, complaint):
+        """
+        Returns the soup of indiviual complaint page to access the detail of complaint. 
+        """
 
-    def add_solved(self, brand, page_soup):
-        solved = page_soup.findAll('article', {'class': 'story-card complaint-card solved ga-v ga-c'})
+        complaint_query = Scraper.__BASE_URL + complaint.find('section').find('h2').find('a').get('href')
+        complaint_page = requests.get(complaint_query, headers=Scraper.__HEADERS)
+        return BeautifulSoup(complaint_page.content, 'html.parser')
 
-        for i in range(len(solved)):
-            complaint_soup = self.change_page(brand, complaint=solved[i])
-            complaint = complaint_soup.find('article', {'class': 'story-primary complaint-card'})
+    def add_data(self, brand, page_soup):
+        """
+        Adding all data for current page soup's all customer complaints. 
+        """
 
-            data = {
-                'brand':           brand,
-                'customer_name':   complaint.find('span', {'class': 'username'}).find('a').text,
-                'customer_link':   complaint.find('span', {'class': 'username'}).find('a').get('href'),
-                'date':            complaint.find('span', {'class': 'username'}).find('span', {'class': 'post-time'}).get('title'),
-                'rating':          len(complaint_soup.find('section', {'class': 'post-setting'}).findAll('span', {'class': 'dark-yellow icomoon-full-star'})),
-                'is_solved':       True,
-                'complaint_title': complaint.find('h1', {'class': 'complaint-title'}).text,
-                'complaint_text':  complaint.find('div', {'class': 'card-text'}).text
-            }
+        complaint_cards = page_soup.findAll('article', {'class': 'story-card complaint-card ga-v ga-c'})
 
-            self.rows.append(data)
+        for i in range(len(complaint_cards)):
+            try:
+                complaint_soup = self.change_complaint_card(complaint_cards[i])
+                complaint = complaint_soup.find('article', {'class': 'story-primary complaint-card'})
 
+                data = {
+                    'brand':           brand,
+                    'customer_name':   complaint.find('span', {'class': 'username'}).find('a').text,
+                    'customer_link':   complaint.find('span', {'class': 'username'}).find('a').get('href'),
+                    'date':            complaint.find('span', {'class': 'username'}).find('span', {'class': 'post-time'}).get('title'),
+                    'rating':          len(complaint_soup.find('section', {'class': 'post-setting'}).findAll('span', {'class': 'dark-yellow icomoon-full-star'})),
+                    'complaint_title': complaint.find('h1', {'class': 'complaint-title'}).text,
+                    'complaint_text':  complaint.find('div', {'class': 'card-text'}).text
+                }
+
+                self.rows.append(data)
+                time.sleep(.5)
+            except Exception as exception:
+                self.exception_counter += 1
+                pass
 
     def scrape_data(self):
+        """
+        Controls current brand name for scraping and changes page number.
+        It also prints information about scraping if argument is passed.
+        """
 
         self.exception_counter = 0
         
         for brand in self.brands:
-            self.page_number = self.start_page
-            
-            while self.page_number <= 2:
-                next_page_soup = self.change_page(brand)
+            page_number = self.start_page
 
-                try:
-                    if self.complaint_type == 'both':
-                        self.add_solved(brand, next_page_soup)
-                        self.add_unsolved(brand, next_page_soup)
-                    elif self.complaint_type == 'solved':
-                        self.add_solved(brand, next_page_soup)
-                    else:
-                        self.add_unsolved(brand, next_page_soup)
-                except Exception:
-                    self.exception_counter += 1
+            while page_number <= self.end_page:
 
-                self.page_number += 1
+                if self.info:
+                    self.print_info(brand, page_number)
 
+                next_page_soup = self.change_page(brand, page_number)
+                self.add_data(brand, next_page_soup)
 
-    def print_info(self):
-        # TODO
-        pass
+                page_number += 1
 
+            print(f'Number of exceptions: {self.exception_counter}')
+        
+        self.save_data()
 
-    def save_data(self, path, file_type):
-        #TODO
-        pass
+    def print_info(self, brand, page_number):
+        print(f'For \"{brand}\" brand, page {page_number} is processing...')
+        print(f'Current total number of complaints scraped: {len(self.rows)}\n')
+        
+
+    def save_data(self):
+        """
+        Saves scraped data to desired format (csv or excel) for output path.
+        If any location or extension is not passed, then default ones are used ('./' and 'csv').
+        It also creates directory for given path if the directory is not created yet.
+        """
+        # create pandas frame to save data 
+        df = pd.DataFrame(self.rows)
+
+        # separate the directory name and file name
+        directory_name, file_name = os.path.split(self.output_path)
+
+        # check if directory exists
+        if not os.path.exists(directory_name):
+            os.makedirs(directory_name)
+
+        try:
+            if self.file_extension == 'csv':
+                df.to_csv(os.path.join(directory_name, file_name + ".csv"), index=False)
+            else:
+                df.to_excel(os.path.join(directory_name, file_name + ".xlsx"), index=False)
+            print("Successfully saved.")
+        except Exception as err:
+            print("Couldn't saved for unexpected reason.")
+        
 
--- start.py
+++ start.py
@@ -4,19 +4,20 @@ from scraper import Scraper
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-b', '--brands', nargs='+', required=True)
-parser.add_argument('-c', '--complaint-type', choices=['unsolved', 'solved', 'both'], default='unsolved')
 parser.add_argument('-s', '--start-page', type=int, default=1)
-parser.add_argument('-e', '--end-page', type=int, default=500)
-parser.add_argument('-o', '--output-path')
-parser.add_argument('-f', '--file-output-type', choices=['csv', 'xlsl'], default='csv')
-parser.add_argument('-i', '--info')
+parser.add_argument('-e', '--end-page', type=int, default=10)
+parser.add_argument('-o', '--output-path', default='./complaints')
+parser.add_argument('-f', '--file-extension', choices=['csv', 'xlsx'], default='csv')
+parser.add_argument('-i', '--info', action='store_true')
 
 args = parser.parse_args()
 
 scraper = Scraper(
     args.brands, 
-    args.complaint_type, 
     args.start_page, 
-    args.end_page)
+    args.end_page,
+    args.info,
+    args.file_extension,
+    args.output_path)
 
 scraper.scrape_data()
