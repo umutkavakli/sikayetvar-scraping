60371f0fa163764881954228458bd52373262409
umutkavakli <umutkavakli.ukk@gmail.com>
2023-03-04 06:08:25 +0300

    adding readme file and editing parameter helpers

---
+++ README.md
@@ -0,0 +1,38 @@
+# Sikayetvar Scraping
+
+A scraping tool for customer complaints of specified brands. 
+<hr>
+
+
+## Usage
+
+Clone repository:
+```
+git clone https://github.com/umutkavakli/sikayetvar-scraping.git
+```
+Move to project directory:
+```
+cd sikayetvar-scraping/
+```
+Run format:
+```
+python3 start.py [-h] -b BRANDS [BRANDS ...] [-s START_PAGE] [-e END_PAGE] [-o OUTPUT_PATH] [-f {csv,xlsx}] [-i]
+```
+
+## Parameters
+
+<b>-b, &nbsp;&nbsp;--brands</b>: Specified brands to scrape complaint data. It is required parameter and it takes at least one brand name. It can take list of brand names.
+<br><b>-s, &nbsp;&nbsp;--start-page</b>: Start page. Default: 1.
+<br><b>-e, &nbsp;&nbsp;--end-page</b>: End page. Default: 10.
+<br><b>-o, &nbsp;&nbsp;--output-path</b>: Output path for saving data. Given dir1/dir2/file_name input created dir1 and dir2 directories (if they don't exist) and a file.
+<br><b>-f, &nbsp;&nbsp;--file-extension</b>: Output extension format. Both 'csv' and 'xlsx' can be used. Default is csv.
+<br><b>-i, &nbsp;&nbsp;--info</b>: Prints information about scraping data for current brand every page.  
+
+## Example
+
+```
+python3 start.py -b brand1 brand2 brand3 -s 10 -e 20 -o my_directory/complaints -f xlsx
+```
+
+<b>Output:</b>
+<br>Creates complaints.xlsx file inside my_directory directory for the data of brand1 brand2 and brand3 between 10 and 20 pages.
--- start.py
+++ start.py
@@ -4,17 +4,17 @@ from scraper import Scraper
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-b', '--brands', nargs='+', required=True, 
-                    help="brands that are used to scrape data")
+                    help="Specified brands to scrape complaint data. It is required parameter and it takes at least one brand name. It can take list of brand names.")
 parser.add_argument('-s', '--start-page', type=int, default=1, 
-                    help="start page (between 1-500). default: 1")
+                    help="Start page. Default: 1.")
 parser.add_argument('-e', '--end-page', type=int, default=10, 
-                    help="end page (between 1-500). default: 10")
+                    help="End page. Default: 10.")
 parser.add_argument('-o', '--output-path', default='./complaints', 
-                    help="outpuh for saving data. dir1/dir2/file_name can be created nested two directory and a file")
+                    help="Output path for saving data. Given dir1/dir2/file_name input created dir1 and dir2 directories (if they don't exist) and a file.")
 parser.add_argument('-f', '--file-extension', choices=['csv', 'xlsx'], default='csv', 
-                    help="output extension format")
+                    help="Output extension format. Both 'csv' and 'xlsx' can be used. Default is csv.")
 parser.add_argument('-i', '--info', action='store_true',
-                    help="prints information for current brand every page")
+                    help="Prints information about scraping data for current brand every page.")
 
 args = parser.parse_args()
 
