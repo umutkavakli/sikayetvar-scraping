d7e5dfaa68d7602896fda2a661b35a11b75b5019
umutkavakli <umutkavakli.ukk@gmail.com>
2023-03-04 05:33:46 +0300

    argument helpers are edited.

--- start.py
+++ start.py
@@ -3,12 +3,18 @@ from scraper import Scraper
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-b', '--brands', nargs='+', required=True)
-parser.add_argument('-s', '--start-page', type=int, default=1)
-parser.add_argument('-e', '--end-page', type=int, default=10)
-parser.add_argument('-o', '--output-path', default='./complaints')
-parser.add_argument('-f', '--file-extension', choices=['csv', 'xlsx'], default='csv')
-parser.add_argument('-i', '--info', action='store_true')
+parser.add_argument('-b', '--brands', nargs='+', required=True, 
+                    help="brands that are used to scrape data")
+parser.add_argument('-s', '--start-page', type=int, default=1, 
+                    help="start page (between 1-500). default: 1")
+parser.add_argument('-e', '--end-page', type=int, default=10, 
+                    help="end page (between 1-500). default: 10")
+parser.add_argument('-o', '--output-path', default='./complaints', 
+                    help="outpuh for saving data. dir1/dir2/file_name can be created nested two directory and a file")
+parser.add_argument('-f', '--file-extension', choices=['csv', 'xlsx'], default='csv', 
+                    help="output extension format")
+parser.add_argument('-i', '--info', action='store_true',
+                    help="prints information for current brand every page")
 
 args = parser.parse_args()
 
